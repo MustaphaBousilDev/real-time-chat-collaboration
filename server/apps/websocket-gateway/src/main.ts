f1dfb271416790447f68a69dcde8506b7e48479c
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-11-09 22:35:05 +0100

    configure KAFKA for microservice gateway socket

--- server/apps/websocket-gateway/src/main.ts
+++ server/apps/websocket-gateway/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { WebsocketGatewayModule } from './websocket-gateway.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(WebsocketGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        //kafka broker
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        // specify uniqueID for consumer
+        groupId: 'websocket-gateway-consumer',
+      },
+    },
+  });
+  app.enableCors();
+  app.useWebSocketAdapter(new IoAdapter(app));
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
--- server/apps/websocket-gateway/src/websocket-gateway.module.ts
+++ server/apps/websocket-gateway/src/websocket-gateway.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { WebsocketGatewayController } from './websocket-gateway.controller';
 import { WebsocketGatewayService } from './websocket-gateway.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [WebsocketGatewayController],
   providers: [WebsocketGatewayService],
 })
